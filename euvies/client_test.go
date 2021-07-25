dca1216ea9f1b3da4e9cdb92a3a82812fcc2d2c5
Zahid Hassan <mdzahidh119@gmail.com>
2021-07-25 15:00:08 +0800

    add euvies soap client tests

--- euvies/client_test.go
+++ euvies/client_test.go
@@ -4,13 +4,45 @@ import (
 	"testing"
 
 	"github.com/DiscoFighter47/vatid-validator/euvies"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestCheckVat(t *testing.T) {
 	client := euvies.NewClient()
 
-	t.Log(client.CheckVat(&euvies.CheckVatReq{
-		CountryCode: "DE",
-		VatNumber:   "266182271",
-	}))
+	Convey("valid vat id", t, func() {
+		resp, err := client.CheckVat(&euvies.CheckVatReq{
+			CountryCode: "DE",
+			VatNumber:   "266182271",
+		})
+		Convey("error should be nil", func() {
+			So(err, ShouldBeNil)
+			Convey("valid should be true", func() {
+				So(resp.Valid, ShouldBeTrue)
+			})
+		})
+	})
+
+	Convey("invalid vat id", t, func() {
+		resp, err := client.CheckVat(&euvies.CheckVatReq{
+			CountryCode: "DE",
+			VatNumber:   "000000000",
+		})
+		Convey("error should be nil", func() {
+			So(err, ShouldBeNil)
+			Convey("valid should be false", func() {
+				So(resp.Valid, ShouldBeFalse)
+			})
+		})
+	})
+
+	Convey("invalid country id", t, func() {
+		_, err := client.CheckVat(&euvies.CheckVatReq{
+			CountryCode: "BD",
+			VatNumber:   "266182271",
+		})
+		Convey("should be an error", func() {
+			So(err, ShouldBeError)
+		})
+	})
 }
